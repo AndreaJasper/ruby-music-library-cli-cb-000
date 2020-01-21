ad8bdfaf5ce32639a832abdf678f75ae4f7c23c1
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-01-21 22:20:18 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,8 +1,26 @@
-require 'pathname'
+class MusicImporter 
+  
+  attr_accessor 
+  
 
-class MusicImporter
-  def initialize(pn)
-    @pn = Pathname("/db/mp3s")
+  def initialize(path)
+    @path = path 
   end
-
+  
+  def path 
+    @path 
+  end
+  
+  def files
+    Dir["#{path}/**/*.mp3"].map{|mp3| mp3.gsub("#{path}/","")}
+  end
+  
+  def import 
+    files.each do |f|
+    Song.create_from_filename(f)
+  end 
+end
+  
+  
+  
 end
