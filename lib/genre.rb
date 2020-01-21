481d965937fb91c4813c25f0bf6f04f0ca45d96b
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-01-21 22:20:56 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,48 +1,47 @@
-require 'pry'
-
-class Genre
+class Genre 
   extend Concerns::Findable
-  attr_accessor :name, :songs
+
+attr_accessor :name, :songs
+
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @songs = []
+  end 
+
+def save
+  @@all << self 
+end 
+
+def self.all
+  @@all 
+end
+
+def self.destroy_all
   @@all = []
+end 
 
-  def initialize(name)
-     @name = name
-     @songs = []
-     @@all << self
-  end
+    def self.create(name)
+genre = self.new(name)
+genre.save
+genre
+end
 
-  def self.all
-    @@all
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(name)
-    genre = self.new(name)
-    genre.save
-    genre
-  end
-
-  def songs
-    @songs
-  end
+def songs 
+  @songs 
+end
 
   def artists
-    artist_array = []
-    @songs.map do |song|
-      if artist_array.include?(song.artist)
-        nil
-      else
-        artist_array << song.artist
-      end
+    array = []
+    @songs.map do |song| 
+    if array.include?(song.artist)
+      nil 
+    else
+      array << song.artist
     end
-    artist_array
+    end 
+    array
   end
 
-
-  def self.destroy_all
-    @@all = []
-  end
 end
